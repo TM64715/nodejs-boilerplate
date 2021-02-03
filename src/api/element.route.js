90d224ec85089375bf2d1b843cdc8586c63a2c85
TM64715 <tage.mehta@gmail.com>
2021-02-03 09:26:02 -0500

    Revert "added frontend"
    This reverts commit 84db9a48ff3577994d8a7891389cc8e108090872.

--- src/api/element.route.js
+++ src/api/element.route.js
@@ -1,8 +1,2 @@
 import { Router } from "express"
-import ElementController from "./elements.controller";
 const router = new Router();
-
-router.post("/save", ElementController.index)
-router.get("/all", ElementController.retrieve);
-router.delete("/delete", ElementController.deleteById);
-export default router;
